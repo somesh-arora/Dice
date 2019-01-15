2db9995e1c4615f0f78302d566ec5827071732c4
Somesh Arora <somesh.arora@mavs.uta.edu>
2019-01-15 00:39:36 -0600

    Added functionality Added Auto Layout Added Motion Detection

--- Dice/ViewController.swift
+++ Dice/ViewController.swift
@@ -9,15 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var randomDiceIndex1: Int = 0
+    var randomDiceIndex2: Int = 0
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
 
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
+    @IBOutlet weak var scoreLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateDiceImages()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        updateDiceImages()
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
+        updateDiceImages()
+    }
+    func updateDiceImages() {
+        randomDiceIndex1 = Int.random(in: 0...5)
+        randomDiceIndex2 = Int.random(in: 0...5)
+        
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
+        scoreLabel.text = "Score: " + String(randomDiceIndex1 + randomDiceIndex2 + 2)
     }
     
 }
